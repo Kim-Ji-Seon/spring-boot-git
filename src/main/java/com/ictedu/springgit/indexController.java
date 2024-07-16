69f8b2e00ce40c91f112fde88a2c6108936c41ba
KimJiSeon <exsillis@naver.com>
2024-07-16 12:00:17 +0900

    "/" 라우팅용 IndexController 추가

---
+++ src/main/java/com/ictedu/springgit/indexController.java
@@ -0,0 +1,10 @@
+package com.ictedu.springgit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class indexController {
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
